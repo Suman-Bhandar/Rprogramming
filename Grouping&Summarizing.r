8bfcf397129241dee72603919c1ee80493209a3b
Suman Bhandari <38178385+bhandari954@users.noreply.github.com>
2020-03-28 15:17:05 +0530

    Push

--- Grouping&Summarizing.r
+++ Grouping&Summarizing.r
@@ -43,5 +43,16 @@ by_year <- gapminder %>%
 ggplot(by_year, aes(x = year, y = medianLifeExp)) +
   geom_point() +
   expand_limits(y = 0)
+
+# Summarize medianGdpPercap within each continent within each year: by_year_continent
+# Summarize medianGdpPercap within each continent within each year: by_year_continent
+by_year_continent<-gapminder%>%
+  group_by(continent,year)%>%summarize(medianGdpPercap=median(gdpPercap))
+
+# Plot the change in medianGdpPercap in each continent over time
+ggplot(by_year_continent,aes(x=year,y=medianGdpPercap,color=continent))+geom_point()+expand_limits(y=0)
+
+
+
                                   
                                   
