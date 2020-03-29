4465fa7ce7323d7ed650709f5ba80e34ad50fdd8
Suman Bhandari <38178385+bhandari954@users.noreply.github.com>
2020-03-29 14:47:27 +0530

    Push

--- Grouping&Summarizing.r
+++ Grouping&Summarizing.r
@@ -53,6 +53,19 @@ by_year_continent<-gapminder%>%
 ggplot(by_year_continent,aes(x=year,y=medianGdpPercap,color=continent))+geom_point()+expand_limits(y=0)
 
 
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+head(gapminder)
+# Summarize the median GDP and median life expectancy per continent in 2007
+by_continent_2007<-gapminder%>%filter(year==2007)%>%
+  group_by(continent)%>%
+  summarize(medianLifeExp=median(lifeExp),medianGdpPercap=median(gdpPercap))
 
-                                  
+# Use a scatter plot to compare the median GDP and median life expectancy
+ggplot(by_continent_2007,aes(x=medianGdpPercap,y=medianLifeExp,color=continent))+
+  geom_point()+expand_limits(y=0)
+
+
+
                                   
