f06470668b50dbca4953b30257401d0771bb5d4f
Suman Bhandari <38178385+bhandari954@users.noreply.github.com>
2020-04-18 23:04:44 +0530

    push

---
+++ Hierarchicalclustering.r
@@ -0,0 +1,58 @@
+# Calculate the Distance
+dist_players <- dist(lineup)
+
+# Perform the hierarchical clustering using the complete linkage
+hc_players <- hclust(dist_players, method = "complete")
+
+# Calculate the assignment vector with a k of 2
+clusters_k2 <- cutree(hc_players, k = 2)
+
+# Create a new data frame storing these results
+lineup_k2_complete <- mutate(lineup, cluster = clusters_k2)
+
+# Count the cluster assignments
+count(lineup_k2_complete, cluster)
+
+# Plot the positions of the players and color them using their cluster
+ggplot(lineup_k2_complete, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
+
+
+# Prepare the Distance Matrix
+dist_players <- dist(lineup)
+
+# Generate hclust for complete, single & average linkage methods
+hc_complete <- hclust(dist_players,method="complete")
+hc_single <- hclust(dist_players,method="single")
+hc_average <- hclust(dist_players,method="average")
+
+
+# Plot & Label the 3 Dendrograms Side-by-Side
+# Hint: To see these Side-by-Side run the 4 lines together as one command
+par(mfrow = c(1,3))
+plot(hc_complete, main = 'Complete Linkage')
+plot(hc_single, main = 'Single Linkage')
+plot(hc_average, main = 'Average Linkage')
+
+dist_players <- dist(lineup, method = 'euclidean')
+hc_players <- hclust(dist_players, method = "complete")
+
+# Calculate the assignment vector with a h of 20
+clusters_h20 <- cutree(hc_players, h = 20)
+
+# Create a new data frame storing these results
+lineup_h20_complete <- mutate(lineup, cluster = clusters_h20)
+
+# Calculate the assignment vector with a h of 40
+clusters_h40 <- cutree(hc_players, h = 40)
+
+# Create a new data frame storing these results
+lineup_h40_complete <- mutate(lineup, cluster = clusters_h40)
+
+# Plot the positions of the players and color them using their cluster for height = 20
+ggplot(lineup_h20_complete, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
+
+# Plot the positions of the players and color them using their cluster for height = 40
+ggplot(lineup_h40_complete, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
